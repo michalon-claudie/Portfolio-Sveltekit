20a1017d3ffa93ba35ce63d89713a043193611a0
Claudie.Michalon <michalon.claudie@outlook.fr>
2024-06-02 11:08:41 +0200

    add img and add slider

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,11 @@
   import { AppBar } from '@skeletonlabs/skeleton';
 </script>
 
-<div class="bg-tertiary-500">
-<AppBar class="bg-gradient-to-r from-primary-500 to-warning-500">
-<svelte:fragment slot="lead">(icon)</svelte:fragment>
-(title)
-<svelte:fragment slot="trail">(actions)</svelte:fragment>
-</AppBar>
-
+<div class="bg-primary-300">
+  <AppBar class="bg-gradient-to-r from-primary-400 to-secondary-300">
+    <svelte:fragment slot="lead">(icon)</svelte:fragment>
+    (title)
+    <svelte:fragment slot="trail">(actions)</svelte:fragment>
+  </AppBar>
 <slot />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+import Imageslider from '$lib/assets/alliMobile.webp'  
+import ImageSlider2 from '$lib/assets/alligator.webp'
+import ImageSlider3 from '$lib/assets/bg-blue.jpg'	
+
+let elemCarousel: HTMLDivElement;
+const unsplashIds = [Imageslider, ImageSlider2, ImageSlider3];
+
+function carouselLeft(): void {
+	const x =
+		elemCarousel.scrollLeft === 0
+			? elemCarousel.clientWidth * elemCarousel.childElementCount // loop
+			: elemCarousel.scrollLeft - elemCarousel.clientWidth; // step left
+	elemCarousel.scroll(x, 0);
+}
+
+function carouselRight(): void {
+	const x =
+		elemCarousel.scrollLeft === elemCarousel.scrollWidth - elemCarousel.clientWidth
+			? 0 // loop
+			: elemCarousel.scrollLeft + elemCarousel.clientWidth; // step right
+	elemCarousel.scroll(x, 0);
+}
+													
+</script>
+
+<div class="card p-4 grid grid-cols-[auto_1fr_auto] gap-4 items-center">
+	<!-- Button: Left -->
+	<button type="button" class="btn-icon variant-filled" on:click={carouselLeft}>
+		<i class="fa-solid fa-arrow-left" />
+	</button>
+	<!-- Full Images -->
+	<div bind:this={elemCarousel} class="snap-x snap-mandatory scroll-smooth flex overflow-x-auto">
+		{#each unsplashIds as img}
+			
+		{/each}
+	</div>
+	<!-- Button: Right -->
+	<button type="button" class="btn-icon variant-filled" on:click={carouselRight}>
+		<i class="fa-solid fa-arrow-right" />
+	</button>
+</div>
+
+
+
