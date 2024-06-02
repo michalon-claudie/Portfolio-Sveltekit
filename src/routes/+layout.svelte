bd6f9defa8594cb1feabaaa861c162cb7ca97b83
Claudie.Michalon <michalon.claudie@outlook.fr>
2024-06-02 12:50:18 +0200

    add h full on content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,18 @@
 <script>
   import "../app.pcss";
   import { AppBar } from '@skeletonlabs/skeleton';
+  import { AppShell } from '@skeletonlabs/skeleton';
 </script>
 
-<div class="bg-primary-300">
-  <AppBar class="bg-gradient-to-r from-primary-400 to-secondary-300">
-    <svelte:fragment slot="lead">(icon)</svelte:fragment>
-    (title)
-    <svelte:fragment slot="trail">(actions)</svelte:fragment>
-  </AppBar>
-<slot />
-</div>
+<AppShell class="bg-primary-300">
+	<svelte:fragment slot="header">
+    <AppBar class="bg-gradient-to-r from-primary-400 to-secondary-300">
+      <svelte:fragment slot="lead">(icon)</svelte:fragment>
+      (title)
+      <svelte:fragment slot="trail">(actions)</svelte:fragment>
+    </AppBar>
+  </svelte:fragment>
+	<svelte:fragment slot="pageHeader">Page Header</svelte:fragment>
+	<slot />
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+</AppShell>
