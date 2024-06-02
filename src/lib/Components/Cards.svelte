d9a59c8aa9edba0cd206c20bd7fff516ce1a03e0
Claudie.Michalon <michalon.claudie@outlook.fr>
2024-06-02 11:40:52 +0200

    add carousel

---
+++ src/lib/Components/Cards.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    export let pictureCards='';
+</script>
+
+<div>
+    <img src={pictureCards} alt="picture slider">
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,44 +2,13 @@
 import Imageslider from '$lib/assets/alliMobile.webp'  
 import ImageSlider2 from '$lib/assets/alligator.webp'
 import ImageSlider3 from '$lib/assets/bg-blue.jpg'	
-
-let elemCarousel: HTMLDivElement;
-const unsplashIds = [Imageslider, ImageSlider2, ImageSlider3];
-
-function carouselLeft(): void {
-	const x =
-		elemCarousel.scrollLeft === 0
-			? elemCarousel.clientWidth * elemCarousel.childElementCount // loop
-			: elemCarousel.scrollLeft - elemCarousel.clientWidth; // step left
-	elemCarousel.scroll(x, 0);
-}
-
-function carouselRight(): void {
-	const x =
-		elemCarousel.scrollLeft === elemCarousel.scrollWidth - elemCarousel.clientWidth
-			? 0 // loop
-			: elemCarousel.scrollLeft + elemCarousel.clientWidth; // step right
-	elemCarousel.scroll(x, 0);
-}
-													
+import Cards from '$lib/Components/Cards.svelte'
+const slideCarousel=[Imageslider,ImageSlider2,ImageSlider3];
 </script>
 
-<div class="card p-4 grid grid-cols-[auto_1fr_auto] gap-4 items-center">
-	<!-- Button: Left -->
-	<button type="button" class="btn-icon variant-filled" on:click={carouselLeft}>
-		<i class="fa-solid fa-arrow-left" />
-	</button>
-	<!-- Full Images -->
-	<div bind:this={elemCarousel} class="snap-x snap-mandatory scroll-smooth flex overflow-x-auto">
-		{#each unsplashIds as img}
-			
-		{/each}
-	</div>
-	<!-- Button: Right -->
-	<button type="button" class="btn-icon variant-filled" on:click={carouselRight}>
-		<i class="fa-solid fa-arrow-right" />
-	</button>
+<div class="snap-x scroll-px-4 snap-mandatory scroll-smooth flex gap-4 overflow-x-auto px-4 py-10">
+	{#each slideCarousel as img}
+		<Cards pictureCards={img}/>
+	{/each}
 </div>
-
-
 
